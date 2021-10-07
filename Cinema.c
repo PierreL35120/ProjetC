d0ee97d1ac0b12268bf96904a39dc890e96474d6
Pierre Lem <72382772+PierreL35120@users.noreply.github.com>
2021-10-07 09:27:04 +0200

    Update Cinema.c

--- Cinema.c
+++ Cinema.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main (void){
+int main(void)
+{
     char voir;
     int age = get_int("Quel est votre âge ?\n"); //Cette étape sert à définir votre âge.
     
-    string filmCategorieEnfant[3]={"Winnie l'ourson", "Fantasia", "Le roi lion"};
-    string filmCategorieAdos[3]={"Madagascar", "One Piece : Strong World", "Gladiator"};
-    string filmCategorieAdulte[3]={"The Shining", "La Nonne", "Conjuring"};
+    string filmCategorieEnfant[3] = {"Winnie l'ourson", "Fantasia", "Le roi lion"};
+    string filmCategorieAdos[3] = {"Madagascar", "One Piece : Strong World", "Gladiator"};
+    string filmCategorieAdulte[3] = {"The Shining", "La Nonne", "Conjuring"};
 
-for (int i=0; i<3; i++){
-    if(age > 18){
-        voir = get_char("Voulez vous voir %s ? y/n ", filmCategorieAdulte[i]);
-    }if(voir =='y'){
-        printf("%s va commencer.", filmCategorieAdulte[i]); //Le [i] change en fonction de la boucle, il adaptera donc le nom du film en fonction de la valeur de [i].
-        break; //Si on accepte, le break va stopper le programme.
+    for (int i = 0; i < 3; i++)
+    {
+        if (age > 18)
+        {
+            voir = get_char("Voulez vous voir %s ? y/n ", filmCategorieAdulte[i]);
+            
+        }
+        if (voir == 'y')
+        {
+            printf("%s va commencer.", filmCategorieAdulte[i]);
+            break; //Si on accepte, le break va stopper le programme.
+        }
+        else if (voir == 'n')
+        {
+            printf("Proposition suivante :\n");
+            i++; //Si jamais on refuse, il ajoute +1 à i, ce qui ferra passer à l'option de film suivant.
+        }
     }
-    else if(voir == 'n'){
-        printf("Proposition suivante :\n");
-    i++; //Si jamais on refuse, il ajoute +1 à i, ce qui ferra passer à l'option de film suivant. Après le 3e film, c'est fini.
-}
-}
 }
